3d8fb1e52a3608718014a2fcb1343b889fbdceaa
Rodrigo Alamo <94248487+Rodrigo9721@users.noreply.github.com>
2023-04-17 12:02:52 -0500

    Update

--- progresol.py
+++ progresol.py
@@ -3,7 +3,7 @@ import json
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'}
 
-url = 'https://progresol.com/'
+url = 'https://progresol.com/' ## Looks like this page is down as of 17/05/2023
 resp = requests.get(url,headers=headers)
 
 start = 'allTiendasPdv = '
